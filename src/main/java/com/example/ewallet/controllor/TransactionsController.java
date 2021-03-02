6c90dcb65e3058ee1b4f6c3a78e74463790ba0cf
Geca13 <karafiloski84@gmail.com>
2021-03-02 16:58:18 +0100

    working on the transfer

--- src/main/java/com/example/ewallet/controllor/TransactionsController.java
+++ src/main/java/com/example/ewallet/controllor/TransactionsController.java
@@ -1,5 +1,7 @@
 package com.example.ewallet.controllor;
 
+import java.time.LocalDateTime;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.repository.query.Param;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -80,7 +82,41 @@ public class TransactionsController {
 		
 		Users recipient = userRepository.findByEmail(email);
         
-		return "transfer";
+		return "redirect:/complete/" + recipient.getId() ;
+	}
+	
+	@GetMapping("/complete/{id}")
+	public String completeTransferPage(Model model, @AuthenticationPrincipal UsersDetails userD,@PathVariable("id") Integer id) {
+		
+		String userEmail = userD.getUsername();
+        Users user = userRepository.findByEmail(userEmail);
+        Users recipient = userRepository.findById(id).get();
+        model.addAttribute("user", user);
+        model.addAttribute("recipient", recipient);
+        model.addAttribute("transaction", new Transaction());
+		return "completeTransfer";
+		
+	}
+	
+	@PostMapping("/complete/{id}")
+	public String transferComplete (Model model, @AuthenticationPrincipal UsersDetails userD, Integer id,@ModelAttribute("transaction") Transaction transaction) {
+		
+		String userEmail = userD.getUsername();
+        Users user = userRepository.findByEmail(userEmail);
+        Users recipient = userRepository.findById(id).get();
+        
+        Transaction transfer = new Transaction();
+        transfer.setTime(LocalDateTime.now());
+        transfer.setDescription("Transfer from " + user.getFirstName() + user.getLastName() + " to " + recipient.getFirstName() + recipient.getLastName());
+        transfer.setUser(recipient);
+		transfer.setAmount(transaction.getAmount());
+		recipient.setMkdBalance(recipient.getMkdBalance() + transaction.getAmount());
+		user.setMkdBalance(user.getMkdBalance() - transaction.getAmount());
+		userRepository.save(recipient);
+		userRepository.save(recipient);
+		transactionRepository.save(transfer);
+		return "redirect:/";
+		
 	}
 
 }
